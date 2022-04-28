c6f7fc1aa5f54cab087adbde479b58e434692539
EvelynGiao <ygao@smith.edu>
2022-04-27 20:37:02 -0400

    Delete colorOff.js

--- colorOff.js
+++
@@ -1,7 +0,0 @@
-
-// This goes in the second web page:
-// Retrieve the sessionStorage variable
-var originalC = sessionStorage.getItem('originalColor');
-
-
-document.body.style.backgroundColor = originalC
